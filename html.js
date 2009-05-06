47f6805bd4a87da328327f4ac9f4c85428d4e3e4
haase <haase@b5240c90-1f15-0410-9d40-e758bbbc7ba7>
2009-05-06 14:09:35 +0000

    Updated copyright notices

--- html.js
+++ html.js
@@ -1,3 +1,38 @@
+/* -*- Mode: Javascript; -*- */
+
+/* Copyright (C) 2009 beingmeta, inc.
+   This file provides for HTML documents using KNOWLETS, including
+    the extraction and processing of embedded KNOWLET definitions
+    or references and interaction with interactive parts of the
+    FDJT library.
+
+   For more information on knowlets, visit www.knowlets.net
+   This library is built on the FDJT (www.fdjt.org) toolkit.
+
+   This program comes with absolutely NO WARRANTY, including implied
+   warranties of merchantability or fitness for any particular
+   purpose.
+
+    Use, modification and redistribution of this program is permitted
+    under the GNU General Public License (GPL) Version 2:
+
+          http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+    Use and redistribution (especially embedding in other
+      CC licensed content) is permitted under the terms of the
+      Creative Commons "Attribution-NonCommercial" license:
+
+          http://creativecommons.org/licenses/by-nc/3.0/ 
+
+    Other uses may be allowed based on prior agreement with
+      beingmeta, inc.  Inquiries can be addressed to:
+
+       licensing@biz.beingmeta.com
+
+   Enjoy!
+
+*/
+
 /* Getting knowdes into HTML */
 
 protoknowde.toHTML=function()
--- knowlets.js
+++ knowlets.js
@@ -1,5 +1,39 @@
-/* A few global variables.  Maybe we should make these fields on Knowlet. */
+/* -*- Mode: Javascript; -*- */
 
+/* Copyright (C) 2009 beingmeta, inc.
+   This file provides a Javascript/ECMAScript of KNOWLETS, 
+     a lightweight knowledge representation facility.
+
+   For more information on knowlets, visit www.knowlets.net
+   For more information about beingmeta, visit www.beingmeta.com
+
+   This library is built on the FDJT (www.fdjt.org) toolkit.
+
+   This program comes with absolutely NO WARRANTY, including implied
+   warranties of merchantability or fitness for any particular
+   purpose.
+
+    Use, modification and redistribution of this program is permitted
+    under the GNU General Public License (GPL) Version 2:
+
+          http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+
+    Use and redistribution (especially embedding in other
+      CC licensed content) is permitted under the terms of the
+      Creative Commons "Attribution-NonCommercial" license:
+
+          http://creativecommons.org/licenses/by-nc/3.0/ 
+
+    Other uses may be allowed based on prior agreement with
+      beingmeta, inc.  Inquiries can be addressed to:
+
+       licensing@biz.beingmeta.com
+
+   Enjoy!
+
+*/
+
+/* A few global variables.  Maybe we should make these fields on Knowlet. */
 var knowlets={};
 var knowlet_nicknames={};
 var protoknowlet={};
